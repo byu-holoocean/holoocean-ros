f95059120e772b68aca1b94a03d14d8a4160ab47
Braden <bjz-github@spwiz.com>
2025-06-21 14:18:08 -0600

    Launching with the param file volumed  in

--- holoocean_main/launch/holoocean_launch.py
+++ holoocean_main/launch/holoocean_launch.py
@@ -2,6 +2,8 @@
 # Author: Braden Meyers
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
@@ -10,23 +12,36 @@ import os
 def generate_launch_description():
     print('Launching HoloOcean Vehicle Simulation')
 
-    package_dir = Path(get_package_share_directory('holoocean_main'))
-    params_file = os.path.join(package_dir, 'config/ros_params.yaml')
+    # Declare a launch argument for the parameter file
+    declare_params_file = DeclareLaunchArgument(
+        'params_file',
+        default_value=str(
+            Path(
+                os.path.join(
+                    get_package_share_directory('holoocean_main'),
+                    'config/ros_params.yaml'
+                )
+            )
+        ),
+        description='Full path to the ROS2 parameters file to use for the node',
+    )
+
+    # Get the launch configuration for the parameter file
+    params_file = LaunchConfiguration('params_file')
 
     holoocean_namespace = 'holoocean'
 
     holoocean_main_node = launch_ros.actions.Node(
         name='holoocean_node',
         package='holoocean_main',
-        executable='holoocean_node',  
+        executable='holoocean_node',
         namespace=holoocean_namespace,
         output='screen',
         emulate_tty=True,
-        parameters=[params_file]  # Pass parameters in the correct format
+        parameters=[params_file],
     )
 
-
     return LaunchDescription([
+        declare_params_file,
         holoocean_main_node,
     ])
-
